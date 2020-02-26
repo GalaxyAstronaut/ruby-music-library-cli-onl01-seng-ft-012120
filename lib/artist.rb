d44465e63040647b01c534e2cd4fce5d183c1954
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-26 18:33:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Artist 
    attr_accessor :name, :songs
   @@all = []
@@ -26,3 +27,53 @@ class Artist
     @@all.detect{|artist| artist.name == name}
   end
 end 
+=======
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    # self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+
+      song.artist = self unless song.artist
+      songs << song unless songs.include?(song)
+      # @songs.uniq!
+
+  end
+
+  def genres
+    # self.songs.collect {|song| song.genre}
+    songs.collect { |song| song.genre}.uniq
+  end
+end
+>>>>>>> 844a2786351977ba519d986d310b28f243ad60ef
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Genre 
    attr_accessor :name, :songs
   @@all = []
@@ -18,4 +19,43 @@ class Genre
     @@all.clear
   end 
   
+=======
+class Genre
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    self.songs.collect {|s| s.artist}.uniq
+  end
+>>>>>>> 844a2786351977ba519d986d310b28f243ad60ef
 end
